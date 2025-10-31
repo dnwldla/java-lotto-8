bb11ab3b64afe35ca3b5a8c3c90a926d1406f202
dnwldla <leeemjw@naver.com>
2025-10-31 15:24:04 +0900

    test: 보너스 번호를 검증하는 테스트 추가

---
+++ src/test/java/lotto/BonusNumberTest.java
@@ -0,0 +1,67 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import lotto.domain.BonusNumber;
+import lotto.domain.MainNumbers;
+import lotto.domain.WinningNumbers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class BonusNumberTest {
+
+    //정상적인 번호
+    @Test
+    void inputWithValidString() {
+        String input = "6";
+
+        assertDoesNotThrow(() -> new BonusNumber(input));
+
+    }
+
+    @Test
+    void inputWithDuplicateNumber() {
+        String input = "6";
+
+        String mainNumbers = "1,2,3,4,5,6";
+
+        IllegalArgumentException exception =
+            assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumbers(new MainNumbers(mainNumbers), new BonusNumber(input)));
+
+        assertThat(exception.getMessage())
+            .isEqualTo(WinningNumbers.ALREADY_EXIST);
+
+    }
+
+    @Test
+    void inputWithInvalidString() {
+        String input = "3!";
+
+        IllegalArgumentException exception =
+            assertThrows(IllegalArgumentException.class, () -> new BonusNumber(input));
+
+        assertThat(exception.getMessage())
+            .isEqualTo(MainNumbers.NOT_INTEGER);
+
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "0",
+        "-1",
+        "46"
+    })
+    void inputWithOutOfRangeNumber(String input) {
+        IllegalArgumentException exception =
+            assertThrows(IllegalArgumentException.class, () -> new BonusNumber(input));
+
+        assertThat(exception.getMessage())
+            .isEqualTo(WinningNumbers.NUMBER_OUT_OF_RANGE);
+    }
+
+}
