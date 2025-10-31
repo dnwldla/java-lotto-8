7ece98eb3534156608ab53c0751dc2262fbf4c3b
dnwldla <leeemjw@naver.com>
2025-10-31 19:09:17 +0900

    test: 로또 상태에 따라 등수를 확인하는 코드 작성

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.domain.BonusNumber;
+import lotto.domain.MainNumbers;
+import lotto.domain.Rank;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class RankTest {
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    void testRank(List<Integer> lottoNums, int bonus, Rank expected) {
+        Lotto lotto = new Lotto(lottoNums);
+        MainNumbers main = new MainNumbers(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(String.valueOf(bonus));
+
+        Rank actual = lotto.getRank(main, bonusNumber);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> cases() {
+        return Stream.of(
+            Arguments.of(List.of(1, 2, 3, 4, 5, 6), 11, Rank.FIRST),
+            Arguments.of(List.of(1, 2, 3, 4, 5, 7), 7, Rank.SECOND),
+            Arguments.of(List.of(1, 2, 3, 4, 5, 7), 11, Rank.THIRD),
+            Arguments.of(List.of(1, 2, 3, 4, 10, 11), 7, Rank.FOURTH),
+            Arguments.of(List.of(1, 2, 3, 10, 11, 12), 7, Rank.FIFTH),
+            Arguments.of(List.of(1, 2, 9, 10, 11, 12), 7, Rank.MISS)
+        );
+
+    }
+}
