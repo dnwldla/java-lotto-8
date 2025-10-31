161b7b6b9891dae038755046f809ac7bb477f259
dnwldla <leeemjw@naver.com>
2025-10-31 14:36:10 +0900

    feat: 당첨번호를 검증하는 로직 추가

--- src/main/java/lotto/domain/MainNumbers.java
+++ src/main/java/lotto/domain/MainNumbers.java
@@ -1,21 +1,60 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class MainNumbers {
+    public static final String INVALID_COUNT = "총 6개의 숫자를 입력해야 합니다";
+    public static final String NUMBER_OUT_OF_RANGE = "숫자는 1부터 45 중 하나입니다";
+    public static final String NOT_INTEGER = "정수가 아닙니다";
+
+    private static final String REGEX = ",";
 
     private final List<Integer> numbers;
 
-    public MainNumbers(List<Integer> numbers) {
-        this.numbers = numbers;
+    public MainNumbers(String input) {
+        this.numbers = parseNumbers(input);
     }
 
     public boolean containsBonusNumber(BonusNumber bonus) {
         Set<Integer> set = new HashSet<>(numbers);
 
         return set.contains(bonus.getNumber());
+    }
+
+
+    public static List<Integer> parseNumbers(String input) {
+        String[] tokens = input.trim().split(REGEX);
+
+        if (tokens.length != LottoConstant.COUNT) {
+            throw new IllegalArgumentException(INVALID_COUNT);
+        }
+
+        return Arrays.stream(input.trim().split(REGEX))
+            .map(MainNumbers::validateInteger)
+            .map(MainNumbers::validateRange).toList();
+    }
+
+
+    private static int validateRange(int number) {
+        if (number < LottoConstant.MIN_NUMBER || number > LottoConstant.MAX_NUMBER) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE);
+        }
+
+        return number;
+    }
+
+    private static int validateInteger(String token) {
+        int number;
+        try {
+            number = Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INTEGER);
+        }
+
+        return number;
     }
 
 }
