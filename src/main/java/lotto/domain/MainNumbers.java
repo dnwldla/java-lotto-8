f32fd033b513d243d32070cbcd602082bab7e620
dnwldla <leeemjw@naver.com>
2025-11-01 22:50:38 +0900

    feat: 당첨번호가 중복일 떄 예외를 반환하는 함수 추가

--- src/main/java/lotto/domain/MainNumbers.java
+++ src/main/java/lotto/domain/MainNumbers.java
@@ -6,9 +6,12 @@ import java.util.Objects;
 import java.util.Set;
 
 public class MainNumbers {
+    public static final String DUPLICATED_NUMBER = LottoConstant.toMessage("서로 다른 당첨번호를 입력해야 합니다");
+
     private final List<Integer> numbers;
 
     public MainNumbers(List<Integer> numbers) {
+        validateDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -31,5 +34,13 @@ public class MainNumbers {
         }
 
         return hit;
+    }
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException(MainNumbers.DUPLICATED_NUMBER);
+        }
     }
 }
