c30540b03a0eaf8f3fefd52d3e585d4690e2c52d
dnwldla <leeemjw@naver.com>
2025-11-03 10:31:42 +0900

    feat: 중복된 로또번호가 존재하는 예외사항 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,12 +15,18 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        Set<Integer> set=new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 존재합니다");
+        }
+
     }
 
     public List<Integer> getNumbers() {
